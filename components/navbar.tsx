9d868d8d99627740309435a6fbe2c6ce74735849
Neil Gebhard <neilgebhard@gmail.com>
2023-08-08 23:06:26 +1000

    Downgrade next for lucide-react compatibility

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,27 @@
 import { UserButton } from '@clerk/nextjs'
-import { Tektur } from 'next/font/google'
+import { Roboto_Mono } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { BatteryCharging } from 'lucide-react'
 
-const tektur = Tektur({ subsets: ['latin'], weight: '900' })
+const roboto = Roboto_Mono({ subsets: ['latin'], weight: '700' })
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center gap-2 p-4 border-b'>
-      <h1 className={cn('font-bold text-2xl', tektur.className)}>
-        AI Therapist
-      </h1>
-      <UserButton />
+    <nav className='flex justify-between items-center gap-2 py-2 px-4 border-b'>
+      <Link href='/'>
+        <h1 className={cn('font-bold text-2xl', roboto.className)}>
+          AI Therapist
+        </h1>
+      </Link>
+      <div className='flex items-center gap-2'>
+        <Button size='sm'>
+          <BatteryCharging className='w-4 h-4 mr-2' />
+          Get Ad-Free
+        </Button>
+        <UserButton />
+      </div>
     </nav>
   )
 }
