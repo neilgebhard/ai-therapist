53e686b135369a5687ba38b44a6f9206d032594b
Neil Gebhard <neilgebhard@gmail.com>
2023-08-09 17:54:57 +1000

    Add a Therapist prisma model

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,25 +5,30 @@ import { cn } from '@/lib/utils'
 import { Button } from './ui/button'
 import { BatteryCharging } from 'lucide-react'
 import { ModeToggle } from './mode-toggle'
+import Container from './container'
 
 const roboto = Roboto_Mono({ subsets: ['latin'], weight: '700' })
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center gap-2 py-2 px-4 border-b'>
-      <Link href='/'>
-        <h1 className={cn('font-bold text-2xl', roboto.className)}>
-          AI Therapist
-        </h1>
-      </Link>
-      <div className='flex items-center gap-2'>
-        <Button size='sm' variant='secondary'>
-          <BatteryCharging className='w-5 h-5 mr-2' />
-          Get Ad-Free
-        </Button>
-        <ModeToggle />
-        <UserButton />
-      </div>
+    <nav className='py-2 border-b'>
+      <Container>
+        <div className='flex justify-between items-center gap-2'>
+          <Link href='/'>
+            <h1 className={cn('font-bold text-2xl', roboto.className)}>
+              AI Therapist
+            </h1>
+          </Link>
+          <div className='flex items-center gap-2'>
+            <Button size='sm' variant='secondary'>
+              <BatteryCharging className='w-5 h-5 mr-2' />
+              Get Ad-Free
+            </Button>
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </div>
+      </Container>
     </nav>
   )
 }
